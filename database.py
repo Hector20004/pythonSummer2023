b64d7cf9c9e71158acdfe60faf548f99f5810a1c
Hector Gonzalez <Hector.Gonzalez049@mymdc.net>
2023-07-07 11:36:56 -0400

    create account ready

--- database.py
+++ database.py
@@ -1,6 +1,22 @@
 import pyodbc
 
+
 def init():
     connection_string = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:hector.database.windows.net,1433;Database=encryptions;Uid=CloudSA28d7309b;Pwd={Console_encrypt};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
     connection = pyodbc.connect(connection_string)
-    return connection.cursor()
+    return connection
+
+def create_account(cursor):
+    while True:
+        username = input('Enter username\n')
+        cursor.execute("select user_name from user_info where user_name = ?",username)
+        record = cursor.fetchone()
+        if record is not None:
+            print('Username already in use')
+            continue
+        else:
+            break
+    while True:
+        password = input('Enter password\n')
+        cursor.execute('Insert into user_info(user_name,user_password) Values(?,?)',(username,password))
+        return 0
--- run.py
+++ run.py
@@ -1,5 +1,17 @@
 import database as DB
 
-cursor = DB.init()
-
+connection = DB.init()
+cursor = connection.cursor()
 print('Welcome to Cmail')
+
+while True:
+    command = input('To login press 1 \nTo create an account press 2 \nTo quit press q\n')
+    if command == '2':
+        DB.create_account(cursor)
+    if command == 'q':
+        querie = cursor.execute('select user_name from user_info').fetchall()
+        for row in querie:
+            print(row)
+        break
+connection.commit()
+connection.close()
