9f518429cefd6e6624056d6b7c7348eb257fb3e6
Hector Gonzalez <Hector.Gonzalez049@mymdc.net>
2023-07-07 15:14:24 -0400

    login account1

--- database.py
+++ database.py
@@ -8,7 +8,9 @@ def init():
 
 def create_account(cursor):
     while True:
-        username = input('Enter username\n')
+        username = input('Enter username or press b to go back\n')
+        if username == 'b':
+            return
         cursor.execute("select user_name from user_info where user_name = ?",username)
         record = cursor.fetchone()
         if record is not None:
@@ -20,3 +22,16 @@ def create_account(cursor):
         password = input('Enter password\n')
         cursor.execute('Insert into user_info(user_name,user_password) Values(?,?)',(username,password))
         return 0
+def login(cursor):
+    while True:
+        username = input('Enter your username or press b to go back\n')
+        if username == 'b':
+            return
+        user = cursor.execute("select user_name,user_password from user_info where user_name = ?",username).fetchall()
+        password = input('Enter your password\n')
+        if user is None:
+            print('Invalid username')
+        else:
+            print(user)
+            break
+
--- run.py
+++ run.py
@@ -8,10 +8,9 @@ while True:
     command = input('To login press 1 \nTo create an account press 2 \nTo quit press q\n')
     if command == '2':
         DB.create_account(cursor)
+    if command == '1':
+        DB.login(cursor)
     if command == 'q':
-        querie = cursor.execute('select user_name from user_info').fetchall()
-        for row in querie:
-            print(row)
         break
 connection.commit()
 connection.close()
